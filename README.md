0a96b2c05ec001cd937cb761af8cf59de6e204af
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-26 21:09:52 +0100

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# linre
+
+## Scripted steps
+
+```
+sudo apt install git -y
+git clone https://github.com/khzs/linre.git
+source linre/...
+```
---
+++ linre-debian-gnome.sh
@@ -0,0 +1,39 @@
+#
+
+wget https://www.scootersoftware.com/DEB-GPG-KEY-scootersoftware.asc
+sudo mv DEB-GPG-KEY-scootersoftware.asc /etc/apt/trusted.gpg.d/
+wget https://www.scootersoftware.com/scootersoftware.list
+sudo mv scootersoftware.list /etc/apt/sources.list.d/
+sudo apt update
+sudo apt install bcompare -y
+
+
+sudo apt install gparted podman-compose doublecmd-qt -y
+
+sudo apt install gdm3 gnome-core gnome-software-plugin-flatpak -y
+sudo apt install gnome-tweaks -y
+# gnome-tweaks : Startup applications
+sudo apt install gnome-shell-extension-appindicator gnome-shell-extension-prefs -y
+
+sudo apt remove firefox-esr -y
+sudo apt autoremove -y
+sudo apt modernize-sources -y
+
+
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close:'
+gsettings set org.gnome.software download-updates-notify false
+
+
+
+# ezeket lehetne fullra scriptelni, meg nincs kesz
+
+# set up ralt
+sudo apt install keyd -y
+sudo micro /etc/keyd/default.conf
+sudo systemctl enable keyd
+sudo systemctl restart keyd
+
+
+
+
+source linre.sh
