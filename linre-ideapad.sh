f8db2424c534633076938a54c86ff16b66dce9f8
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-26 21:09:52 +0100

    .

---
+++ linre-ideapad.sh
@@ -0,0 +1,18 @@
+#
+
+#
+flatpak install org.qbittorrent.qBittorrent org.jellyfin.JellyfinServer -y
+
+# usb portba dugott egerre wakeup meglegyen
+lsusb | grep Xiaomi
+echo 'ACTION=="add", KERNEL=="1-3", SUBSYSTEM=="usb", ATTR{power/wakeup}="enabled"' | sudo tee /etc/udev/rules.d/90-usb-wakeup.rules
+
+
+
+
+# ezeket lehetne fullra scriptelni, meg nincs kesz
+
+# grub
+sudo mv /etc/grub.d/30_os-prober /etc/grub.d/09_os-prober
+sudo micro /etc/default/grub
+sudo update-grub
---
+++ linre.sh
@@ -0,0 +1,26 @@
+#
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install com.microsoft.Edge com.github.PintaProject.Pinta -y
+
+git clone https://github.com/khzs/dotfiles-core.git ~/.config/dotfiles-core
+source ~/.config/dotfiles-core/one-time-setup-$(uname).sh 
+
+sudo ln -s /home/linuxbrew/.linuxbrew/bin/bat /usr/local/bin/bat
+sudo ln -s /home/linuxbrew/.linuxbrew/bin/micro /usr/local/bin/micro
+sudo ln -s /home/linuxbrew/.linuxbrew/bin/mc /usr/local/bin/mc
+
+
+
+
+# MS Edge "Save as" fix
+# https://blog.rdiez.es/p/solucionar-el-cierre-de-microsoft-edge-en-linux-wayland-al-descargar-archivos/
+
+
+
+
+# app indicator + gnome extensionsnek kell egy logout-login, vagy reboot, es azutan jelennek meg a listaban
+# MS Edge "Save as" fixnek is kell egy logout-login
+# zsh default shellnek is kell egy logout-login
+
+sudo reboot
