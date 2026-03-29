92062c862e9fc3a22f2494cfe48378937c81da6a
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 20:16:09 +0200

    gnome software notif disable++

--- linre-debian-gnome.sh
+++ linre-debian-gnome.sh
@@ -18,9 +18,9 @@ sudo apt modernize-sources -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 
-
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close:'
 gsettings set org.gnome.software download-updates-notify false
+gsettings set org.gnome.software allow-updates false
 
 ./resources/gext install    start-overlay-in-application-view@Hex_cz \
                             blur-my-shell@aunetx \
