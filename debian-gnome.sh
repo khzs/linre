0ad9cc12675cd86215db2d892ee5d30d038aa08f
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 11:52:16 +0200

    gext + gnome-extensions config

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -5,6 +5,11 @@ sudo apt install gdm3 gnome-core -y
 sudo apt remove firefox-esr gnome-software -y
 
 
+./resources/gext install    just-perfection-desktop@just-perfection \
+                            blur-my-shell@aunetx \
+                            appindicatorsupport@rgcjonas.gmail.com \
+                            start-overlay-in-application-view@Hex_cz
+
 dconf load /org/gnome/ < resources/gnome-all.ini
 curl -fsSL https://raw.githubusercontent.com/khzs/keyboard-everything/main/screenshot.ini | dconf load /org/gnome/
 
