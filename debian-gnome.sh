40c890e5cdcafb1bc37e4dee24489e5889c9f1ec
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 11:52:16 +0200

    gext + gnome-extensions config

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -1,10 +1,14 @@
 #
 
 sudo apt install gparted doublecmd-qt -y
-sudo apt install gdm3 gnome-core gnome-software-plugin-flatpak -y
-sudo apt install gnome-shell-extension-appindicator gnome-shell-extension-prefs -y
+sudo apt install gdm3 gnome-core gnome-shell-extension-prefs gnome-software-plugin-flatpak -y
 sudo apt remove firefox-esr -y
 
+
+./resources/gext install    start-overlay-in-application-view@Hex_cz \
+                            blur-my-shell@aunetx \
+                            just-perfection-desktop@just-perfection \
+                            appindicatorsupport@rgcjonas.gmail.com
 
 dconf load /org/gnome/ < resources/gnome-all.ini
 
