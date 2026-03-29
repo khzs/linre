42aa88c6eab9624de79706bda24ddba54e6af698
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 20:49:56 +0200

    gnome-all config via dconf

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -6,6 +6,8 @@ sudo apt install gnome-shell-extension-appindicator gnome-shell-extension-prefs 
 sudo apt remove firefox-esr -y
 
 
+dconf load /org/gnome/ < resources/gnome-all.ini
+
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close:'
 gsettings set org.gnome.software download-updates-notify false
 
