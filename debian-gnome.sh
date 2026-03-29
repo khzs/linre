2096fc44660f374c4e3d02c9ac709a253cb6ba91
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 20:49:56 +0200

    gnome-all config via dconf

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -5,6 +5,9 @@ sudo apt install gdm3 gnome-core -y
 sudo apt remove firefox-esr gnome-software -y
 
 
+dconf load /org/gnome/ < resources/gnome-all.ini
+curl -fsSL https://raw.githubusercontent.com/khzs/keyboard-everything/main/screenshot.ini | dconf load /org/gnome/
+
 gsettings set org.gnome.software download-updates-notify false
 
 
