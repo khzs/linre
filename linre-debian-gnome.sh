ef7dd73a12d2dbd9bfbadae0a4af1201e75e3d21
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-03-29 20:16:09 +0200

    gnome software notif disable++

--- linre-debian-gnome.sh
+++ linre-debian-gnome.sh
@@ -21,6 +21,7 @@ sudo apt modernize-sources -y
 
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close:'
 gsettings set org.gnome.software download-updates-notify false
+gsettings set org.gnome.software allow-updates false
 
 ./resources/gext install    start-overlay-in-application-view@Hex_cz \
                             blur-my-shell@aunetx \
