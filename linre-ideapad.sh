e75048e2c16b739b07e64c2e5e8280f0d31fbeab
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-02 21:14:05 +0200

    grub

--- linre-ideapad.sh
+++ linre-ideapad.sh
@@ -1,6 +1,5 @@
 #
 
-#
 sudo apt install podman-compose keyd gnome-tweaks -y                # gnome-tweaks : Startup applications
 flatpak install org.localsend.localsend_app org.qbittorrent.qBittorrent org.jellyfin.JellyfinServer -y
 
@@ -12,12 +11,6 @@ sudo systemctl restart keyd
 lsusb | grep Xiaomi
 echo 'ACTION=="add", KERNEL=="1-3", SUBSYSTEM=="usb", ATTR{power/wakeup}="enabled"' | sudo tee /etc/udev/rules.d/90-usb-wakeup.rules
 
-
-
-
-# ezeket lehetne fullra scriptelni, meg nincs kesz
-
-# grub
 sudo mv /etc/grub.d/30_os-prober /etc/grub.d/09_os-prober
-sudo micro /etc/default/grub
+sudo patch --forward --reject-file=- /etc/default/grub resources/grub.patch
 sudo update-grub
