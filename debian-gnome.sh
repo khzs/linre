975b37a1df84aa2067c8b17093ff37c516288f44
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-02 20:57:59 +0200

    keyd

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -1,6 +1,6 @@
 #
 
-sudo apt install flatpak gparted doublecmd-qt gnome-shell-extension-prefs -y
+sudo apt install flatpak gparted keyd doublecmd-qt gnome-shell-extension-prefs -y
 sudo apt install gdm3 gnome-core -y
 sudo apt remove firefox-esr gnome-software -y
 
@@ -14,6 +14,12 @@ dconf load /org/gnome/ < resources/gnome-all.ini
 curl -fsSL https://raw.githubusercontent.com/khzs/keyboard-everything/main/screenshot.ini | dconf load /org/gnome/
 
 gsettings set org.gnome.software download-updates-notify false
+
+
+sudo mkdir -p /etc/keyd/
+sudo curl -fsSL https://raw.githubusercontent.com/khzs/keyboard-everything/main/default.conf -o /etc/keyd/default.conf
+sudo systemctl enable keyd
+sudo systemctl restart keyd
 
 
 source omnibus.sh
