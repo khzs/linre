58a2ede761b848ee937dd6086c2962dc6e4b9fc9
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-11 13:59:46 +0200

    MS Edge fix

--- linre.sh
+++ linre.sh
@@ -9,14 +9,10 @@ sudo ln -s /home/linuxbrew/.linuxbrew/bin/bat /usr/local/bin/bat
 sudo ln -s /home/linuxbrew/.linuxbrew/bin/micro /usr/local/bin/micro
 sudo ln -s /home/linuxbrew/.linuxbrew/bin/mc /usr/local/bin/mc
 
-
-
-
 # MS Edge "Save as" fix
 # https://blog.rdiez.es/p/solucionar-el-cierre-de-microsoft-edge-en-linux-wayland-al-descargar-archivos/
-
-
-
-
+cp /var/lib/flatpak/exports/share/applications/com.microsoft.Edge.desktop ~/.local/share/applications/
+patch --forward --reject-file=- ~/.local/share/applications/com.microsoft.Edge.desktop resources/edge.desktop.patch
+update-desktop-database ~/.local/share/applications
 
 sudo reboot
