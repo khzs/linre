e6e6005ed969dc4dadd2b8d5ea1fd139f71d2a05
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-17 19:38:40 +0200

    entry script 1.1

--- anchor1.sh
+++ anchor1.sh
@@ -6,6 +6,7 @@ newgrp sudo << 'EOF'
 sudo apt install git -y
 git clone https://github.com/khzs/linre.git && cd linre
 source debian-gnome.sh
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 source omnibus.sh
 sudo reboot
 EOF
